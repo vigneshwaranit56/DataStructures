ebc753513facd81a48567a30da4d5fdd7240f5a9
vignesh <vignesh.s@tringapps.com>
2018-07-01 21:17:39 -0700

    add tree data structures

---
+++ src/com/nonlinear/tree/Tree.java
@@ -0,0 +1,23 @@
+package com.nonlinear.tree;
+
+public interface Tree {
+	
+	public  void preOrder(Node node);
+
+	public  void inOrder(Node node);
+
+	public void postOrder(Node node);
+
+	public  void levelOrder(Node node);
+
+	public  Node delete(Node node, Integer value);
+
+	Node insert(Node node, Integer key);
+
+	Boolean search(Node node, Integer key);
+	
+	Integer height(Node node);
+
+	Boolean isEmpty(Node node);
+
+}
