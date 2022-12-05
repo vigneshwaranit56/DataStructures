2acecb88279482cf11dc4daebb4231f1c768cf9c
vigneshwaranit56 <vigneshwaranit56@gmail.com>
2022-12-05 20:55:38 +0530

    upgrade maven

---
+++ src/main/com/linear/stack/StackI.java
@@ -0,0 +1,18 @@
+package com.practice.linear.stack;
+
+public interface StackI<T> {
+	
+	void push(T data) throws Exception; // insert
+
+	T pop()throws Exception; //delete
+
+	T peek(); // last inserted data
+
+	Boolean isEmpty(); // empty?
+
+	Boolean isFull(); // full?
+	
+	void print(); // print entire 
+	
+	Integer size(); // size
+}
