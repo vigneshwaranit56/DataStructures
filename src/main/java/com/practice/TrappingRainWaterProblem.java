9a7e016a47e84fffc43238764accb654511d81ee
vigneshwaranit56 <vigneshwaranit56@gmail.com>
2023-06-19 20:03:33 +0530

    42. Trapping Rain Water

--- src/main/java/com/practice/TrappingRainWaterProblem.java
+++ src/main/java/com/practice/TrappingRainWaterProblem.java
@@ -3,6 +3,9 @@ package com.practice;
 public class TrappingRainWaterProblem {
     public int solution(int[] height) {
 
+        for (int i =0;i<height.length;i++){
+
+        }
 
         return 0;
     }
