9875ab606a3ca23934800f7cd90f9a0873b0049e
vigneshwaranit56 <vigneshwaranit56@gmail.com>
2023-06-23 20:49:07 +0530

    42. Trapping Rain Water

--- src/main/java/com/practice/TrappingRainWaterProblem.java
+++ src/main/java/com/practice/TrappingRainWaterProblem.java
@@ -14,15 +14,48 @@ public class TrappingRainWaterProblem {
                 if (rMax.getAsInt() < highest) {
                     highest = rMax.getAsInt();
                 }
-            //    if (rMax.isPresent() && rMax.getAsInt() >= highest) {
-                    waterLevel += highest - height[i];
-           //     }
+                //    if (rMax.isPresent() && rMax.getAsInt() >= highest) {
+                waterLevel += highest - height[i];
+                //     }
 
             } else
                 highest = height[i];
         }
 
         return waterLevel;
+    }
+
+    public int solution2(int[] height) {
+
+//        4,2,0,3,2,5
+        int left = 0, right = height.length - 1;
+        int leftMax = height[0],rightMax= height[height.length-1];
+        int waterLevel = 0;
+
+        while(left < right){
+
+         if(leftMax < rightMax){
+             left++;
+             if(leftMax < height[left]){
+                 leftMax=height[left];
+             }else{
+                 waterLevel+=leftMax-height[left];
+             }
+         }else{
+             right--;
+             if(rightMax < height[right]){
+                 rightMax = height[right];
+             }else{
+                 waterLevel+=rightMax-height[right];
+             }
+         }
+
+        }
+
+
+
+        return waterLevel;
+
     }
 }
 
