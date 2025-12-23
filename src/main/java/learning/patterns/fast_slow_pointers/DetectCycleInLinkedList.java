ad821228a5eab480bdf8f0e9e06e17bbd8eaea39
vigneshwaranit56 <vigneshwaranit56@gmail.com>
2025-12-23 21:55:21 +0530

    Day 4 Coding patterns fast and slow pointers and longest detect cycle

--- src/main/java/learning/patterns/fast_slow_pointers/DetectCycleInLinkedList.java
+++ src/main/java/learning/patterns/fast_slow_pointers/DetectCycleInLinkedList.java
@@ -1,7 +1,22 @@
 package learning.patterns.fast_slow_pointers;
 
+import learning.linear.linkedlist.LinkdedList;
+import learning.linear.linkedlist.SinglelyLinkList;
+import learning.linear.list.singlylinklist.Node;
+
 public class DetectCycleInLinkedList {
     public static void main(String[] args){
+        LinkdedList list = new SinglelyLinkList();
+        list.add(6);
+        list.add(3);
+        list.add(4);
+
+        list.add(2);
+        list.add(7);
+        list.add(9);
+        list.add(8);
+        list.add(5);
+        list.add(6);
 
     }
 }
